dc8e364e6c1e40e32cd213d183133fef121eb2af
David Dyer <davidrossdyer@gmail.com>
2021-09-23 14:26:22 -0400

    created .js, moved index.html to main

---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+// Time/Date goes inside <p> tag w/n <header>
+var clock = moment();
+$("#currentDay").text(clock.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+// isn't doing anything
+// class="lead"
