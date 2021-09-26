7b610dda20746e4d91a5ffee14353fd45e2d5bf8
David Dyer <davidrossdyer@gmail.com>
2021-09-26 11:24:49 -0400

    still working on color change function

--- Develop/script.js
+++ Develop/script.js
@@ -1,65 +1,171 @@
 // Time/Date goes inside <p> tag w/n <header>
 timeInterval = setInterval(function () {
   var currentDay = moment();
-  $("#currentDay").text(currentDay.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+  $("#currentDay").text(currentDay.format("dddd, MMMM Do YYYY, h:mm:ss A"));
 }, 1000);
 
-// Current hour of the day
-var currentHour = moment().format("h");
+// // Current hour of the day
+var currentHour = moment().format("hA");
 
-// Separate Variable for each hour block
-var hourStr7a = $("#hour7a").text();
-var hourStr8a = $("#hour8a").text();
-var hourStr9a = $("#hour9a").text();
-var hourStr10a = $("#hour10a").text();
-var hourStr11a = $("#hour11a").text();
-var hourStr12p = $("#hour12p").text();
-var hourStr1p = $("#hour1p").text();
-var hourStr2p = $("#hour2p").text();
-var hourStr3p = $("#hour3p").text();
-var hourStr4p = $("#hour4p").text();
-var hourStr5p = $("#hour5p").text();
-var hourStr6p = $("#hour6p").text();
-var hourStr7p = $("#hour7p").text();
+// Separate variable for each hour, targets text
+var hour9a = $("#hour9a").text();
+var hour10a = $("#hour10a").text();
+var hour11a = $("#hour11a").text();
+var hour12p = $("#hour12p").text();
+var hour1p = $("#hour1p").text();
+var hour2p = $("#hour2p").text();
+var hour3p = $("#hour3p").text();
+var hour4p = $("#hour4p").text();
+var hour5p = $("#hour5p").text();
 
-function stringTime(hourStr) {
-  if (hourStr.length == 3) {
-    hourStrFinal = "0" + hourStr;
-  }
-  hourStr = hourStrFinal[0] + hourStrFinal[1];
-  console.log(hourStr);
-  return;
+console.log(hour9a);
+
+// var timeBlock9a = moment("9AM", "hA");
+// var timeBlock10a = moment("10AM", "hA");
+// var timeBlock11a = moment("11AM", "hA");
+// var timeBlock12p = moment("12PM", "hA");
+// var timeBlock1p = moment("1PM", "hA");
+// var timeBlock2p = moment("2PM", "hA");
+// var timeBlock3p = moment("3PM", "hA");
+// var timeBlock4p = moment("4PM", "hA");
+// var timeBlock5p = moment("5PM", "hA");
+
+// console.log(timeBlock5p);
+
+// var currentHour = moment().format("hA");
+console.log(currentHour);
+// console.log(timeBlock9a.isBefore(currentHour));
+// console.log(timeBlock10a.isBefore(currentHour));
+// console.log(timeBlock11a.isBefore(currentHour));
+// console.log(timeBlock12p.isBefore(currentHour));
+// console.log(timeBlock1p.isBefore(currentHour));
+// console.log(timeBlock2p.isBefore(currentHour));
+// console.log(timeBlock3p.isBefore(currentHour));
+// console.log(timeBlock4p.isBefore(currentHour));
+// console.log(timeBlock5p.isBefore(currentHour));
+
+// If future
+if (hour9a > currentHour) {
+  $("#event9a").attr("style", "background-color: #77dd77");
+}
+// If present
+if (hour9a === currentHour) {
+  $("#event9a").attr("style", "background-color: #ff6961");
+}
+// // If passed
+if (hour9a < currentHour) {
+  $("#event9a").attr("style", "background-color: #d3d3d3");
 }
 
-stringTime(hourStr7a);
-stringTime(hourStr8a);
-stringTime(hourStr9a);
-stringTime(hourStr10a);
-stringTime(hourStr11a);
-stringTime(hourStr12p);
-stringTime(hourStr1p);
-stringTime(hourStr2p);
-stringTime(hourStr3p);
-stringTime(hourStr3p);
-stringTime(hourStr4p);
-stringTime(hourStr5p);
-stringTime(hourStr6p);
-stringTime(hourStr7p);
+// If future
+if (hour10a > currentHour) {
+  $("#event10a").attr("style", "background-color: #77dd77");
+}
+// If present
+if (hour10a === currentHour) {
+  $("#event10a").attr("style", "background-color: #ff6961");
+}
+// // If passed
+if (hour10a < currentHour) {
+  $("#event10a").attr("style", "background-color: #d3d3d3");
+}
+
+// If future
+if (hour11a > currentHour) {
+  $("#event11a").attr("style", "background-color: #77dd77");
+}
+// If present
+if (hour11a === currentHour) {
+  $("#event11a").attr("style", "background-color: #ff6961");
+}
+// // If passed
+if (hour11a < currentHour) {
+  $("#event11a").attr("style", "background-color: #d3d3d3");
+}
+
+// If future
+if (hour12p > currentHour) {
+  $("#event12p").attr("style", "background-color: #77dd77");
+}
+// If present
+if (hour12p === currentHour) {
+  $("#event12p").attr("style", "background-color: #ff6961");
+}
+// // If passed
+if (hour12p < currentHour) {
+  $("#event12p").attr("style", "background-color: #d3d3d3");
+}
+
+// If future
+if (hour1p > currentHour) {
+  $("#event1p").attr("style", "background-color: #77dd77");
+}
+// If present
+if (hour1p === currentHour) {
+  $("#event1p").attr("style", "background-color: #ff6961");
+}
+// // If passed
+if (hour12p < currentHour) {
+  $("#event1p").attr("style", "background-color: #d3d3d3");
+}
+
+// If future
+if (hour2p > currentHour) {
+  $("#event2p").attr("style", "background-color: #77dd77");
+}
+// If present
+if (hour2p === currentHour) {
+  $("#event2p").attr("style", "background-color: #ff6961");
+}
+// // If passed
+if (hour2p < currentHour) {
+  $("#event2p").attr("style", "background-color: #d3d3d3");
+}
+
+// If future
+if (hour3p > currentHour) {
+  $("#event3p").attr("style", "background-color: #77dd77");
+}
+// If present
+if (hour3p === currentHour) {
+  $("#event3p").attr("style", "background-color: #ff6961");
+}
+// // If passed
+if (hour3p < currentHour) {
+  $("#event3p").attr("style", "background-color: #d3d3d3");
+}
+
+// If future
+if (hour4p > currentHour) {
+  $("#event4p").attr("style", "background-color: #77dd77");
+}
+// If present
+if (hour4p === currentHour) {
+  $("#event4p").attr("style", "background-color: #ff6961");
+}
+// // If passed
+if (hour4p < currentHour) {
+  $("#event4p").attr("style", "background-color: #d3d3d3");
+}
+
+// If future
+if (hour5p > currentHour) {
+  $("#event5p").attr("style", "background-color: #77dd77");
+}
+// If present
+if (hour5p === currentHour) {
+  $("#event5p").attr("style", "background-color: #ff6961");
+}
+// // If passed
+if (hour5p < currentHour) {
+  $("#event5p").attr("style", "background-color: #d3d3d3");
+}
 
 // Lock button saves to local storage
-$(".saveBtn").on("click", storeEvent);
+// $(".saveBtn").on("click", storeEvent);
 
-function storeEvent() {
-  var hour = $(".hour");
-  var description = $(".description");
-  localStorage.setItem("event", hour, description);
-}
-
-// Change background color of event block if in the past
-// $(".description").attr("style", "background-color: #d3d3d3");
-
-// Change background color of event block if in the present
-// $(".description").attr("style", "background-color: #ff6961");
-
-// Change background color of event block if in the future
-// $(".description").attr("style", "background-color: #77dd77");
+// function storeEvent() {
+//   var hour = $(".hour");
+//   var description = $(".description");
+//   localStorage.setItem("event", hour, description);
+// }
